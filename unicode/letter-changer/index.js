102e6eae92b09a71706b2c1beebdc6a5f758be1f
Ashvin-Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2020-07-06 11:08:36 -0700

    Update index.js

--- unicode/letter-changer/index.js
+++ unicode/letter-changer/index.js
@@ -1,1 +1,25 @@
 
+let chars = {
+	"A" : u"\u0410",
+	"a" : u"\u0430",
+	"B" : u"\u0412",
+	"b" : u"\u042a",
+	"C" : u"\u0421",
+	"c" : u"\u0441",
+	"D" : u"\u0430",
+	"d" : u"\u0500",
+	"E" : u"\u0500",
+};
+
+document.getElementById("sub").addEventListener('click', function(){
+    var txt = "";
+    for(var i = 0; i < document.getElementById("inp").length; i ++){
+      try{
+        txt += chars[txt];
+      }
+      catch{
+        txt += txt.charAt(i);
+      }
+    }
+    document.getElementById("textout").innerHTML = txt;
+});
